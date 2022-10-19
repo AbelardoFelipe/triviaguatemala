faf1211b15e9b9cadf373dfede91d6a9cab8be1b
piezadigital <wil.vs@outlook.com>
2022-10-18 18:50:32 -0600

    added puntos migrate

--- database/migrations/2022_10_18_214203_puntos.php
+++ database/migrations/2022_10_18_214203_puntos.php
@@ -14,6 +14,7 @@ class Puntos extends Migration
     public function up()
     {
         Schema::create('puntos', function (Blueprint $table) {
+        $table->engine = 'InnoDB';
         $table->id();
         $table->integer('punto');
         $table->integer('nivel');
