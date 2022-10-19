f2677e51afde279def969b34698f8f34e7160e9c
piezadigital <wil.vs@outlook.com>
2022-10-19 11:53:24 -0600

    update pregunta model

--- app/Models/Pregunta.php
+++ app/Models/Pregunta.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Pregunta extends Model
 {
     use HasFactory;
+
+    protected $table = "preguntas";
+
+    protected $fillable = ['punot', 'nivel', 'intento', 'numero_pregunta'];
+    protected $hidden = ['id','user_id'];
+
+    public function obtenerAPregunta()
+        {
+            return Pregunta::all();
+        }
+
+    public function obtenerPreguntaPorId($id)
+        {
+            return Pregunta::find($id);
+        }
+
 }
