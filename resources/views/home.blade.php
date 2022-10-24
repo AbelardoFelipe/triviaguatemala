bb86ea49657c2fa5f9f191938409157ade079414
piezadigital <wil.vs@outlook.com>
2022-10-23 19:52:38 -0600

    update relation table for auth user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,17 @@
 <div class="dashboard">
     <div class="dash-1">
         <canvas id="boy" width="300" height="500"></canvas>
+        <span>{{$pregunta}}</span>      
     </div>
     <div class="dash-2">
-        @php
-            if($pregunta == 0){                
-                echo "<a href='./preguntas') }}'> <i class='fas fa-play-circle'></i></a>";
-            }else{             
-                echo "<a href='./preguntas/$pregunta')'><i class='fas fa-play-circle'></i> </a>";
-            }
-            
-        @endphp        
+        @empty($pregunta)
+            <a href="{{URL::to('/preguntas/1')}}"><i class='fas fa-play-circle'></i> </a>
+        @endempty
+        
+        @if($pregunta <10)
+            @php($contador = $pregunta);
+            <a href="{{URL::to('/preguntas/'.++$contador)}}"><i class='fas fa-play-circle'></i> </a>                
+        @endif        
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,11 +81,11 @@
                             <div class="menu-datos-2A">
                                 <p>Nivel</p>   
                                 <i class="fas fa-trophy"></i>
-                                <p data-user-nivel="{{ $punteo[0]->nivel ?? 1}}" data-user-numero_pregunta="{{ $punteo[0]->numero_pregunta ?? 0}}"><span id="show-nivel">{{ $pregunta ?? 0}}</span>/10</p>
+                                <p data-user-nivel="{{ $punteo[0]->nivel ?? 1}}" data-user-numero_pregunta="{{ ++$pregunta ?? 1}}"><span id="show-nivel">@if($pregunta ?? 1 <=10 ) {{ $pregunta}} @else {{ $pregunta--}} @endif</span>/10</p>
                             </div> 
                             <div class="menu-datos-2B">
                                 <div id="progressBar">
-                                    <div id="progressBarFill"></div>     
+                                    <div data-user-aprobado="{{$aprobado}}" id="progressBarFill"></div>     
                                 </div>
                             </div>
                         </div>                            
