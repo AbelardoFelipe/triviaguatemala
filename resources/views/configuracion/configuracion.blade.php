ed91785b286ec6185ffed0aec8a6c164a9c98469
piezadigital <wil.vs@outlook.com>
2022-11-02 12:34:42 -0600

    update form config

--- resources/views/configuracion/configuracion.blade.php
+++ resources/views/configuracion/configuracion.blade.php
@@ -7,22 +7,32 @@
                     <span>Recibir notificaciones por correo.</span>
                     <div class="switch-button">
                         <!-- Checkbox -->
-                        <input type="checkbox" name="switch-button" id="switch-label" class="switch-button__checkbox">
+                        <input type="checkbox" name="switch-button-email" id="switch-label-email" class="switch-button__checkbox">
                         <!-- Botón -->
-                        <label for="switch-label" class="switch-button__label"></label>
+                        <label for="switch-label-email" class="switch-button__label"></label>
                     </div>
                 </div>
                 <div class="group-form">
                     <label for="">Quitar música de fondo.</label>
-                    
+                    <div class="switch-button">
+                        <!-- Checkbox -->
+                        <input type="checkbox" name="switch-button-music-background" id="switch-label-music-background" class="switch-button__checkbox">
+                        <!-- Botón -->
+                        <label for="switch-label-music-background" class="switch-button__label"></label>
+                    </div>
                 </div>
                 <div class="group-form">
                     <label for="">Quitar sonido </label>
-                    
+                    <div class="switch-button">
+                        <!-- Checkbox -->
+                        <input type="checkbox" name="switch-button-sound-efect" id="switch-label-sound-efect" class="switch-button__checkbox">
+                        <!-- Botón -->
+                        <label for="switch-label-sound-efect" class="switch-button__label"></label>
+                    </div>
                 </div>
                 <div class="group-form">
                     <label for="">Actualizar memória cache (minutos)</label>
-                    
+                    <input type="range">
                 </div>
             </form>
         </div>
