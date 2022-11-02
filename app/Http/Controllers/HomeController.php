19e661a69eae2a76fff72adf3d3916dbf9207d20
piezadigital <wil.vs@outlook.com>
2022-11-02 10:22:02 -0600

    add function to cache from home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Pregunta;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 class HomeController extends Controller
 {
     
@@ -29,6 +31,24 @@ class HomeController extends Controller
     {   // Get the currently authenticated user's ID...
         $id_auth = Auth::id();
         //---------------------------------------------
+        $nivel = 10;
+        $cache_time = 1440;
+        $expiresAt = Carbon::now()->addMinutes($cache_time);
+
+        if(($nivel-9 ) <= $nivel ){       
+            if(Cache::has($nivel)){
+                for($x=1; $x<=$nivel; $x++){
+                    $preguntasArray = Cache::get($x);
+                }                        
+            }else{
+                for($i=1; $i<=$nivel; $i++){
+                    $preguntas = Http::get('http://ec2-44-203-35-246.compute-1.amazonaws.com/preguntas.php?nivel='.$i.'&grupo=4');
+                    $preguntasArray = $preguntas->json();
+                    Cache::put($i,$preguntasArray, $expiresAt ?? null);
+                }               
+            }
+        }    
+
         $punteoArray = Pregunta::all();
         $punteo = json_decode($punteoArray);    
         $punto = DB::table('puntos')->where('user_id', '=', $id_auth)->where('punto', '=', 5)->sum('punto');
